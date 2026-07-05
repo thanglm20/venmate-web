ce7a945479dc8743321d1b43e5a291baec8ce484
thanglm <thanglm20@github.com>
2026-07-05 21:50:15 +0700

    add qr download app

---
+++ static/config.js
@@ -0,0 +1,4 @@
+const APP_CONFIG = {
+    ios_store_url: "https://apps.apple.com/vn/app/t%C3%ACm-%C4%91%E1%BB%99i/id6767106887",
+    android_store_url: "https://play.google.com/store/apps/details?id=com.venmate.sport_partner_matching_app"
+};
